a14a27eb094bce41a62fad15354b1cf4a1a8b85e
danielBreitlauch <github@flying-stampe.de>
2017-03-30 00:30:22 +0200

    more checks before splitting

--- FoodScan/wunderlist.py
+++ FoodScan/wunderlist.py
@@ -29,7 +29,7 @@ class WuList:
             sub_tasks = self.client.get_task_subtasks(task['id'])
 
         item = Item.parse(task['title'], notes, sub_tasks)
-        self.split_items(item, sub_tasks, unmark)
+        self.split_items(item, task['id'], sub_tasks, unmark)
         return item
 
     def create_item(self, item):
@@ -38,15 +38,28 @@ class WuList:
         for shop_item in item.shop_items:
             self.client.create_subtask(task['id'], unicode(shop_item), completed=shop_item.selected)
 
-    def split_items(self, item, sub_tasks, unmark):
+    def split_items(self, item, iid, sub_tasks, unmark):
+        task = self.client.get_task(iid)
+        if task['completed']:
+            self.logger.info("task got completed before split: " + item.name.encode('utf-8'))
+            return
+
         selected_shop_items = []
         for shop_item in item.shop_items:
             if shop_item.selected:
                 selected_shop_items.append(shop_item)
 
         if len(selected_shop_items) > 0:
-            while len(selected_shop_items) > 1:
+            completed = True
+            for t in sub_tasks:
+                completed = completed and t['completed']
+            if completed:
+                self.logger.info("all subtasks completed before split: " + item.name.encode('utf-8'))
+                return
+
+            while 1 < len(selected_shop_items):
                 shop_item = selected_shop_items.pop()
+                self.logger.info("splitting of: " + shop_item.name.encode('utf-8'))
                 new_item = Item(name=shop_item.name, price=shop_item.price, url=shop_item.link)
                 new_item.select_shop_item(shop_item)
                 self.create_item(new_item)
