87d401f00e156e01d7f1623b04fc4ce264920a06
Daniel Breitlauch <daniel@flying-stampe.de>
2022-01-03 21:12:34 +0100

    add home assistant notification possibility

--- FoodScan/BarcodeSync/barcodeSync.py
+++ FoodScan/BarcodeSync/barcodeSync.py
@@ -3,7 +3,8 @@ import pickle
 from _thread import start_new_thread
 from pysimplelog import Logger
 import traceback
-
+import json
+import requests
 from FoodScan.BarcodeSync import BarcodeReader
 
 
@@ -15,6 +16,11 @@ class BarcodeSync:
         self.shop_list = shopList
         self.file_name = "barcode.db"
         self.matches = self.load()
+        if 'notification_homeassistant_bearer_token' in config:
+            self.notification_url = config['notification_homeassistant_url']
+            self.bearer_token = config['notification_homeassistant_bearer_token']
+
+        self.notify("Booted")
         if asynchron:
             start_new_thread(self.listen, ())
         else:
@@ -61,6 +67,8 @@ class BarcodeSync:
                 traceback.print_exc()
 
     def add_barcode(self, item):
+        self.notify(item.name)
+
         for task, existing in self.shop_list.list_items():
             self.logger.info("Existing item: " + existing.selected_shop_item().name)
             if existing.synced() and item.name.lower() in existing.selected_shop_item().name.lower():
@@ -70,4 +78,29 @@ class BarcodeSync:
                 return
 
         self.shop_list.create_item(item)
+
+    def notify(self, message):
+        if not self.bearer_token:
+            self.logger.debug("no notification credentials provided")
+            return
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self.bearer_token,
+        }
+
+        data = {
+            "title": "Barcode scan",
+            "message": message,
+            "data": {
+                "group": "barcode_scan",
+                "push": {
+                    "interruption-level": "passive",
+                    "sound": "none"
+                }
+            }
+        }
+
+        response = requests.request("POST", self.notification_url, headers=headers, data=json.dumps(data))
+        self.logger.info("Notification " + message + "sent. Response: " + str(response.status_code))
         self.logger.info("Create new item")
